936b9c78083dbdb33f495812d29ff19e95a94337
aayush-brainerhub <aayushc.brainerhub@gmail.com>
2025-10-19 12:36:03 +0530

    changes on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -69,10 +69,9 @@ export default function Footer() {
               </a>
               <a href="https://www.linkedin.com/company/intelligentllm/" className="text-indigo-400 hover:text-white transition-colors" aria-label="Twitter">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.291zM5.005 7.917c-1.401 0-2.195-.87-2.195-1.928 0-1.071.794-1.928 2.222-1.928 1.428 0 2.222.857 2.222 1.928 0 1.058-.794 1.928-2.249 1.928zm-.736 12.535H6.28V9H3.97V20.452z"/>  </svg>
               </a>
-              <a href="#" className="text-indigo-400 hover:text-white transition-colors" aria-label="GitHub">
+              {/* <a href="#" className="text-indigo-400 hover:text-white transition-colors" aria-label="GitHub">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
                     fillRule="evenodd"
@@ -80,7 +79,7 @@ export default function Footer() {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
